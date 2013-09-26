837c0b2c2da23aec9c78f8cc8587edb5f43577ab
Justin Pocta <justinpocta@gmail.com>
2013-09-26 18:33:53 -0400

    whatever

---
+++ site/templates/projects.php
@@ -0,0 +1,20 @@
+<?php snippet('header') ?>
+
+<body id="secondary">
+<section class="content">
+
+  <article>
+
+    <h1><?php echo html($page->title()) ?></h1>
+
+    <?php echo kirbytext($page->text()) ?>
+    <?php echo kirbytext($page->slideshow()) ?>
+    <?php echo kirbytext($page->created()) ?>
+    <?php echo kirbytext($page->credits()) ?>
+
+  </article>
+
+</section>
+
+<?php snippet('all-projects') ?>
+<?php snippet('footer') ?>
