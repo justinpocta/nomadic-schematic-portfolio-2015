b6ca6116414fe9e903ccfc7c06d4407de50acfce
Justin Pocta <justinpocta@gmail.com>
2013-09-26 18:50:06 -0400

    ideas

--- site/templates/project.php
+++ site/templates/project.php
@@ -7,13 +7,14 @@
 
     <h1>PROJECT: <?php echo html($page->title()) ?></h1>
 	
-	<?php echo kirbytext($page->project_type()) ?>
+	Project Type: <?php echo kirbytext($page->project_type()) ?>
 
     <?php echo kirbytext($page->text()) ?>
     <?php echo kirbytext($page->slideshow()) ?> <!-- ? -->
 
-    <?php echo kirbytext($page->created()) ?>
-    <?php echo kirbytext($page->credits()) ?>
+    Created <?php echo kirbytext($page->created()) ?>
+    <a href="<?php echo kirbytext($page->link()) ?>">APP BUTTON or LINK</a>
+    Team: <?php echo kirbytext($page->credits()) ?>
 
   </article>
 
