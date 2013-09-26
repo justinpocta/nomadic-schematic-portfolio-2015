4c677efba8ec0423ce501c8b933427c09812604b
Justin Pocta <justinpocta@gmail.com>
2013-09-26 17:32:53 -0400

    oh right, the images and such

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,13 +2,14 @@
 <html lang="en">
 <head>
   
-  <title><?php echo html($site->title()) ?> - <?php echo html($page->title()) ?></title>
+  <title><?php echo html($site->title()) ?> | <?php echo html($page->title()) ?></title>
   <meta charset="utf-8" />
   <meta name="description" content="<?php echo html($site->description()) ?>" />
   <meta name="keywords" content="<?php echo html($site->keywords()) ?>" />
   <meta name="robots" content="index, follow" />
+  <meta name="viewport" content="width=device-width, user-scalable=false;">
+  <meta name="viewport" content="initial-scale=1, maximum-scale=1">
 
-  <?php echo css('assets/css/styles.css') ?>
   <?php echo css('assets/css/stylesheet.css') ?>
 
 </head>
