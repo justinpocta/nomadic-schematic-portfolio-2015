51004da9112ed125da1e15a11e85b90b57c2fe82
Justin Pocta <justinpocta@gmail.com>
2013-09-26 16:12:54 -0400

    .

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,6 +1,85 @@
   <footer>
-    <?php echo kirbytext($site->copyright()) ?>
-    (link: mailto:justin@nomadic-schematic.com text: contact) (link: http://resume.nomadic-schematic.com text: résumé) (link: http://linkedin.nomadic-schematic.com text: linkedin) (link: http://twitter.com/justinpocta text: twitter) (link: http://dribbble.com/justinpocta text: dribbble) (link: http://github.com/justinpocta text: github) (link: http://instagram.com/justinpocta text: instagram)
+
+	<div class="main-intro" id="main-intro-footer">
+	  <a class="notch" name="bottom" href="#top"></a>
+	  <h2>Nomadic Schematic is the wandering studio of <a class="nowrap" href="/justin-pocta">Justin Pocta</a>, <span class="intro-secondary">an independent designer living in <span class="nowrap">Brooklyn, NY.</span></span></h2>
+	  <hr style="border:1px dotted #FFF;opacity:.1;">
+	  <p>These case studies are from projects created for clients at various studios and <span class="nowrap">in-house</span> at a startup, with a few personal projects intersperse.</p>
+	  
+	  <ul class="exits">
+	    <li><a href="mailto:justin@nomadic-schematic.com">contact</a></li>
+	    <li><a href="http://resume.nomadic-schematic.com">résumé</a></li>
+	    <li><a href="http://linkedin.com/in/justinpocta">linkedin</a></li>
+	    <li><a href="http://twitter.com/justinpocta">twitter</a></li>
+	    <li><a href="http://dribbble.com/justinpocta">dribbble</a></li>
+	    <li><a href="http://github.com/justinpocta">github</a></li>
+	    <li><a href="http://instagram.com/justinpocta">instagram</a></li>
+	  </ul>
+
+	  <a class="show-for-small button-top" href="#start"></a>
+
+	  <div class="nomadic-footer">
+	    <?php echo kirbytext($site->copyright()) ?>
+	  </div>
+	</div>
+
+
+	<script>
+	  // didn't seem to be working
+	  // document.write('<script src=' +
+	  // ('__proto__' in {} ? '/js/vendor/zepto' : '/js/vendor/jquery') +
+	  // '.js><\/script>')
+	</script>
+	  
+	<script src="js/foundation.min.js"></script>
+	  <!--
+	  
+	  <script src="js/foundation/foundation.js"></script>
+	  
+	  <script src="js/foundation/foundation.alerts.js"></script>
+	  
+	  <script src="js/foundation/foundation.clearing.js"></script>
+	  
+	  <script src="js/foundation/foundation.cookie.js"></script>
+	  
+	  <script src="js/foundation/foundation.dropdown.js"></script>
+	  
+	  <script src="js/foundation/foundation.forms.js"></script>
+	  
+	  <script src="js/foundation/foundation.joyride.js"></script>
+	  
+	  <script src="js/foundation/foundation.magellan.js"></script>
+	  
+	  <script src="js/foundation/foundation.orbit.js"></script>
+	  
+	  <script src="js/foundation/foundation.placeholder.js"></script>
+	  
+	  <script src="js/foundation/foundation.reveal.js"></script>
+	  
+	  <script src="js/foundation/foundation.section.js"></script>
+	  
+	  <script src="js/foundation/foundation.tooltips.js"></script>
+	  
+	  <script src="js/foundation/foundation.topbar.js"></script>
+	  
+	  <script src="js/foundation/foundation.interchange.js"></script>
+	  
+	  -->
+
+	<script>
+	  $(document).foundation();
+	</script>
+
+	<script>
+	  // (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  // (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  // m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  // })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  // ga('create', 'UA-339651-6', 'nomadic-schematic.com');
+	  // ga('send', 'pageview');
+	</script>
+
   </footer>
 
 </body>
