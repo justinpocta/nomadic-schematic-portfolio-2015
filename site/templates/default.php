4995955959bf3c2a7262a24bb002e23cb4f54800
Justin Pocta <justinpocta@gmail.com>
2013-09-27 07:35:53 +0000

    fixing via server on VIM, baby

--- site/templates/default.php
+++ site/templates/default.php
@@ -7,17 +7,15 @@ $tags = tagcloud($blog);
 
 ?>
 
-<div id="container">
-	<header>
-<!-- 		<h2>Hello my name is</h2>
-		<h1><?php echo html($site->name()) ?></h1>
-		<h3><?php echo html($site->address()) ?></h3>
-		<div id="info">
-			<a href="mailto:<?php echo html($site->mail()) ?>" title="Contact me"><img src="<?php echo html($site->url()) ?>/assets/images/icons/interface/info.png"></a>
-		</div> -->
-	</header>
-	<div id="main" role="main">
-	  
+<!-- <div id="container"> -->
+
+<div class="main-intro" id="main-intro-header">
+  <div><h2>Nomadic Schematic, <span style="font-family:Baskerville;font-weight:lighter;font-style:italic;">the design portfolio of <a class="nowrap" href="about.html">Justin Pocta</span></a>.</h2></div>
+</div>
+
+<div class="main-nav">
+  <a href="" name="start" style="position:absolute;top:0px;"></a>
+  
 	<!--
 	These are our filter options. The "data-filter" classes are used to identify which
 	grid items to show.
@@ -27,13 +25,13 @@ $tags = tagcloud($blog);
 			<li data-filter="<?php echo $tag->name() ?>"><?php echo $tag->name() ?></li>
 		<?php endforeach ?>	       
 	</ol> -->
-
-	<ul id="nav-list">
+<center>
+	<div id="nav-list"><div id="nav-list-box">
 		<!--
 		These are our grid items. Notice how each one has classes assigned that
 		are used for filtering. The classes match the "data-filter" properties above.
 		-->
-		<?php if(param('tag')) {
+                <?php if(param('tag')) {
 		
 		$articles = $pages->find('articles')
 							->children()
@@ -49,13 +47,13 @@ $tags = tagcloud($blog);
 							->flip();                            
 		
 		} ?>
-		<?php foreach($articles as $article): ?>
-		<li class="link-button button-category <?php foreach(str::split($article->tags()) as $tag): ?><?php echo $tag ?> <?php endforeach ?>">
-			<a href="<?php echo $article->url() ?>" title="<?php echo html($article->title()) ?>"><?php foreach($article->images() as $image): ?><?php echo thumb($image, array('width' => 300, 'quality' => 70)) ?><?php endforeach ?><p><?php echo html($article->title()) ?></p></a></li>             
+		<a href="#bottom" id="nomadic-schematic" class="link-button"><p></p></a>
+		<?php foreach($articles as $article): ?> 
+		<a href="<?php echo $article->url() ?>" title="<?php echo html($article->title()) ?>" class="link-button project-mask"><p><?php echo html($article->title()) ?></p></a>
 		<?php endforeach ?>            
 		<!-- End of grid blocks -->
-	  </ul>
-	  
+	  </div></div>
+	</center>  
 	</div>
 </div>
 
