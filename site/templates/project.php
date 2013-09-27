a87eda694d2fc1a85f85be0c80292f454c088ca2
Justin Pocta <justinpocta@gmail.com>
2013-09-26 23:19:22 -0400

    tests

--- site/templates/project.php
+++ site/templates/project.php
@@ -5,26 +5,23 @@
 
   <article>
 
-    <h1>PROJECT: <?php echo html($page->title()) ?></h1>
-	
-	Project Type: <?php echo kirbytext($page->project_type()) ?>
-
-    <?php echo kirbytext($page->text()) ?>
-    <?php echo kirbytext($page->slideshow()) ?> <!-- ? -->
-
-    TEXT:: <?php echo kirbytext($page->text()) ?>
-    SLIDESHOW:: <?php echo kirbytext($page->slideshow()) ?>
-    CREATED:: <?php echo kirbytext($page->created()) ?>
-    CREDITS:: <?php echo kirbytext($page->credits()) ?>
-
-	TINYURL:: $page->tinyurl()
-	URL:: $page->url()
-	
-	NEXT a:: $page->next($sort='title', $direction='asc')
-	NEXT b:: $page->hasNext($sort='title', $direction='asc')
-
-    LINK:: <a href="<?php echo kirbytext($page->link()) ?>">APP BUTTON or LINK</a>
-
+    <h1>PROJECT: <?php echo html($page->title()) ?></h1> <br>
+	 <br>
+	Project Type: <?php echo kirbytext($page->project_type()) ?> <br>
+	 <br>
+    TEXT:: <?php echo kirbytext($page->text()) ?> <br>
+    SLIDESHOW:: <?php echo kirbytext($page->slideshow()) ?> <br>
+    CREATED:: <?php echo kirbytext($page->created()) ?> <br>
+    CREDITS:: <?php echo kirbytext($page->credits()) ?> <br>
+ 	<br>
+	TINYURL:: $page->tinyurl() <br>
+	URL:: $page->url() <br>
+	 <br>
+	NEXT a:: $page->next($sort='title', $direction='asc') <br>
+	NEXT b:: $page->hasNext($sort='title', $direction='asc') <br>
+ 	<br>
+    LINK:: <a href="<?php echo kirbytext($page->link()) ?>">APP BUTTON or LINK</a> <br>
+ 	<br>
   </article>
 
 </section>
