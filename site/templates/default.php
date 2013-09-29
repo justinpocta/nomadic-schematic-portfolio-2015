2f66e3eba721d9c8cb1046910eaf03ed01b3dd9c
Justin Pocta <justinpocta@gmail.com>
2013-09-29 06:26:10 +0000

    oh. hellz. yeah.

--- site/templates/default.php
+++ site/templates/default.php
@@ -36,7 +36,7 @@ $tags = tagcloud($blog);
 	} ?>
 
 		<?php foreach($projects as $project): ?>
-		<a href="<?php echo $project->url() ?>" style="background-image:url('/assets/img/thumb-<?php echo $project->projecttype ?>_<?php echo $project->thumb ?>')" title="<?php echo html($project->title()) ?>" class="link-button <?php echo $project->projecttype_button ?>"><p><?php echo html($project->title()) ?></p></a>
+		<a href="<?php echo $project->url() ?>" style="background-image:url('/assets/img/thumb-<?php echo $project->projecttype ?>_<?php echo $project->thumb ?><?php echo $project->thumbtype ?>')" title="<?php echo html($project->title()) ?>" class="link-button <?php echo $project->projecttype_button ?>"><p><?php echo html($project->title()) ?></p></a>
 
 		<?php endforeach ?>
 
