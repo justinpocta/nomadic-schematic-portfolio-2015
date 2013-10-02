1b4e24a9164f82513e58f7ab42b9aed0cb17293a
Justin Pocta <justinpocta@gmail.com>
2013-10-02 22:27:07 +0000

    header slide worksssss, now using project.txt for max-width to just get this ish donnnne

--- site/templates/project.php
+++ site/templates/project.php
@@ -5,10 +5,9 @@
 <body id="secondary">
 <section class="content">
 
-
 <!-- hero slideshow -->
 <div class="large-12 small-12 logo-intro">
-  
+
   <a class="go-home" href="/">&laquo; back to projects</a>
   <div>
 <?php if($page->hasPrev()) { ?><a href="<?php echo $page->prev()->url() ?>" class="button-back" style="left:0;" title="Previous"></a>
@@ -19,55 +18,49 @@
     <?php echo $page->title ?> - <?php echo h($page->projecttype) ?>
   </span>
 <?php if($page->hasNext()) { ?><a href="<?php echo $page->next()->url() ?>" class="button-next" style="right:0;" title="Next"></a><?php } else { ?>
-<a href="/" class="button-home-right" title="Home"></a>
+  <a href="/" class="button-home-right" title="Home"></a>
 <?php }; ?>
 </div>
 
 </div>
 
 <!-- objective -->
-<div class="small-12 large-12" style="background-color:rgba(1,71,11,1.0); margin:0 auto; display:block; background-image:url('/assets/img/header-<?php echo $page->projecttype() ?>_<?php echo $page->thumb ?>-bg.jpg'); background-repeat:no-repeat; max-height:500px; <?php echo $page->headercss ?>">
-      
-      
-	<div class="container slidesPrimary" style="max-width:<?php 
-$headimager = $page->files()->find(!preg_match('^header^'));
-echo h($headimager->width()) ?>">
-      
-      <div id="slides">
+<div class="small-12 large-12" style="background-color:rgba(1,71,11,1.0); margin:0 auto; display:block; background-image:url('/assets/img/header-<?php echo $page->projecttype() ?>_<?php echo $page->thumb ?>-bg.jpg'); background-repeat:no-repeat; <?php echo $page->headercss ?>">
 
-	<?php foreach($page->images() as $image) { if(!preg_match('^header-^', $image->filename() )) continue; ?>
-        <div style="background:url('<?php echo $image->url() ?>') no-repeat; background-size:auto; background-position:center center;"></div>
-        <?php } ?>
+     <div class="container slidesPrimary" style="<?php echo $page->headerwidth ?>">
+      <div id="slides" style="<?php echo $page->headerheight ?>">
 
-        <div style="background:url(/assets/img/header-<?php echo $page->projecttype() ?>_<?php echo $page->thumb() ?>.png);"></div>
-        <div style="background:url(/assets/img/header-<?php echo $page->projecttype() ?>_<?php echo $page->thumb() ?>.png);"></div>
+	   <?php foreach($page->images() as $image) { if(!preg_match('^header-^', $image->filename() )) continue; ?>
+        <div style="background:url('<?php echo $image->url() ?>') no-repeat; background-size:auto; background-position:bottom center;"></div>
+      <?php } ?>
+
       </div>
-
     </div>
 
   </div>
+
 <!-- end objective -->
 
   <article>
 
 <!-- content intro -->
 <div class="large-12 small-12" style="padding-top:25px;">
-  
+
   <div class="large-12 small-12"> <!-- MAIN 1 -->
 	<div class="row">
 	<div class="column large-8 small-12"><!--// content 1 - left //-->
 		<span class="detail-title">Project Information</span>
 		<?php echo kirbytext($page->text()) ?>
-	
+
 		<?php if($page->quote()) {?>
 			<div class="quotes"><p><?php echo $page->quote() ?></p></div>
 		<?php } else {}; ?>
-	
+
 <div style="display:block;width:286px;height:602px;background:url('/assets/img/empty-iphone.png') no-repeat;"></div>
 
 	</div>
 	</div>
-	<div class="container slidesSecondary cXolumn">
+	<div class="container slidesSecondary">
 		<div id="slides2">
 		<?php foreach($page->images() as $image) { if(!preg_match('^galleryA-^', $image->filename() )) continue; ?>
 		<div style="background:url('<?php echo $image->url() ?>') no-repeat; background-size:auto; background-position:center center;"></div>
@@ -92,8 +85,8 @@ echo h($headimager->width()) ?>">
     </div><!-- end tags -->
     <div style="float:left;margin-top:10px;width:100%;" class="project-credits">
       <span class="detail-title">Details</span>
-	<small>Completed <?php echo h($page->created()) ?></small>	
-	
+	<small>Completed <?php echo h($page->created()) ?></small>
+
 	<?php echo kirbytext($page->credits()) ?>
 
 	<?php if($page->applink()) {?>
