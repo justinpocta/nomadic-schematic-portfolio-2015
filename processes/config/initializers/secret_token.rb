32fb3febacaae83b6b09355d820b857087ccded5
Justin Pocta <justinpocta@gmail.com>
2013-10-27 20:25:05 +0000

    process NEW

---
+++ processes/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Processes::Application.config.secret_key_base = '7092cdf069b87958fc9f946cecac291a7361cc8bad34dfd01fa2d3ec8b37407ab693afb128fe561ffef082a8f2d5dc2d6a8debd8614d5a348282f4cba932aacf'
